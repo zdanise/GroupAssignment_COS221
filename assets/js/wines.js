ee79b29f305845201af8ea32d1d1e9a4c21a4f49
mamatankane <u22524798@tuks.co.za>
2023-06-06 15:57:41 +0200

    final wines page

--- assets/js/wines.js
+++ assets/js/wines.js
@@ -1,8 +1,8 @@
-let wines=[];
-let orderby='ASC';
-let sortby='wine_name';
-let searchby='';
-let filterby='';
+wines={};
+orderby='ASC';
+sortby='wine_name';
+searchby='';
+filterby='';
 function fetchData() {
     var xhr = new XMLHttpRequest();
     let data = {
@@ -21,7 +21,6 @@ function fetchData() {
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log(xhr.response);
             $('#row2').empty();
             wines = {};
             if (xhr.response == 'No wines found') {
@@ -55,7 +54,7 @@ function loadWines(wine) {
     row2.append('<div class="col-md-4"> \
     <div class="card mb-4 product-wap rounded-0"> \
         <div class="card rounded-0"> \
-            <img class="card-img rounded-0 img-fluid" style="width: 300px;" src='+wine.image+'> \
+            <img class="card-img rounded-0 img-fluid" style="width: 300px; max-height: 700px" src='+wine.image+'> \
             <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center"> \
                 <ul class="list-unstyled"> \
                     <li><a class="btn btn-success text-white mt-2" href="shop-single.html?user_id=' + user_id + '&wine=' + encodeURIComponent(JSON.stringify(wine)) + '"><i class="far fa-eye"></i></a></li> \
@@ -95,14 +94,17 @@ function addToCart(wine_id, amount=1){
         dataType: "json",
         contentType: "application/json",
         success: function(response) {
-            $("#successModal").modal("show");
-            setTimeout(function() {
-              $("#successModal").modal("hide"); 
-            }, 1250);
+          var toast = document.getElementById("successToast");
+          var bsToast = new bootstrap.Toast(toast);
+          bsToast.show();
+          setTimeout(()=>{bsToast.hide();},1000);
         },
         error: function(xhr, status, error) {
         }
       });
+      
+      
+
       
 }
 
@@ -110,6 +112,56 @@ function addWithQ(){
     var quantityInput = document.getElementById("quantity");
     var quantity = quantityInput.value;
     addToCart(wine.wine_id,quantity);
+}
+
+function onSuccess(response){
+    var reviews = response.data;
+    console.log(response);
+    var html = '';
+    if (response.message==undefined){
+
+        reviews.forEach((element) => {
+        var rating = element.rate;
+        var comment = element.comment;
+        var username= element.username;
+        average= element.average_rate;
+        html += '<p><b>User:</b> '+username+'<br> <b>Rating:</b> ' + rating + '<br> <b>Comment:</b> ' + comment + '<br>------<br></p>';
+        });
+        $('#average').html('Average rating: '+average);
+    } 
+    else{
+        html += '<p>'+response.message+'<br></p>';
+    }
+    $('#comments').html(html);
+}
+
+function submitReview(){
+    var comment = document.getElementById("comment").value.trim();
+    var rate = document.getElementById("rate").value;
+    var data = {
+        AddReview:"yes",
+        comment: comment,
+        rate: rate,
+        wine_id: wine.wine_id,
+        user_id: user_id
+      };
+      
+      console.log(data);
+      
+      $.ajax({
+        url: 'review.php',
+        type: 'POST',
+        data: JSON.stringify(data),
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function(response) {
+          onSuccess(response);
+        },
+        error: function(xhr, status, error) {
+          console.error(error);
+        }
+      });
+      
 }
 
 function setSort(i){
--- review.php
+++ review.php
@@ -14,11 +14,14 @@ if(isset($json['BasedOnwine'])) {
 
     $wine_id = $json['wine_id'];
 
-    $sql2 = "SELECT review.rate, review.COMMENT, user.username
-         FROM review
-         JOIN customer ON customer.customer_id = review.customer_id
-         JOIN user ON user.user_id = customer.user_id
-         WHERE review.wine_id = ".$wine_id;
+    $sql2 = "SELECT r.rate, r.wine_id, r.comment, w.wine_name, u.username,
+    ROUND((SELECT AVG(rate) FROM review WHERE wine_id = r.wine_id), 1) AS average_rate
+FROM review r
+JOIN wine w ON r.wine_id = w.wine_id
+JOIN user u ON r.user_id = u.user_id
+WHERE r.wine_id = $wine_id;
+";
+    
 
     $result = $conn->query($sql2);
     $rowCount = mysqli_num_rows($result);
@@ -82,7 +85,11 @@ else if(isset($json['BasedOnUser']))
 
             $user_id= $json['user_id'];
 
-           $sql2 = "SELECT rate, wine_id, COMMENT, (SELECT wine_name FROM wine WHERE wine_id=$wine_id) AS wine_name FROM review WHERE wine_id=$wine_id";
+           $sql2 = "SELECT r.review_id, r.wine_id, r.comment, w.wine_name, ROUND(AVG(r.rate), 1) AS average_rate
+           FROM review r
+           JOIN wine w ON w.wine_id = r.wine_id
+           WHERE r.customer_id = $customer_id
+           GROUP BY r.review_id, r.wine_id, r.comment, w.wine_name";
 
                 $result = $conn->query($sql2);
                 $ReviewItems = array();
@@ -107,7 +114,243 @@ else if(isset($json['BasedOnUser']))
 
       
 }
+else if(isset($json['AddReview']))
+{
+    $wine_id = $json['wine_id'];
+    $user_id = $json['user_id'];
+    $rate= $json['rate'];
+    $comment= $json['comment'];
+    //check if user is a customer
+    // $sqlCheck="SELECT customer_id FROM customer Where user_id=$user_id LIMIT 1"; new update: they dont have to be a customer to review a wine
+    // $result = $conn->query($sqlCheck);
+   
+    //     $rowCount = mysqli_num_rows($result);
+    //     if ($rowCount=== 0) {
+    //         $send = array(
+    //             "type" => "Notcustomer",
+    //             "message"=>"User has never purchased before therefore user cannot write a review."
+                
+    //         );
+          
+    //         header('Content-Type: application/json');
+    //         echo json_encode( $send);
+    //     }
+    //     else{
+    //         //extracting customer id
+    //         $row = mysqli_fetch_assoc($result);
+    //         $customer_id = $row['customer_id']; 
+    //         $wine_id=$json['wine_id'];
+    //         if(isset($json['comment']))
+    //         {
+    //             $comment = $json['comment'];
+    //         }
+    //         else
+    //         {
+    //             $comment ="no comment";
+    //         }
+    //         if(isset($json['rate']) && $json['rate']!=null )
+    //         {
+    //             $rate = $json['rate'];
+    //         }
+    //         else
+    //         {
+    //             $rate=0;
+    //         }
+            
+              
+            
+    
 
+    $sql1 = 'INSERT INTO review ( wine_id, rate, comment, user_id)
+    VALUES (?, ?, ?, ?)';
+
+    $stmt = mysqli_stmt_init($conn);
+
+    if (mysqli_stmt_prepare($stmt, $sql1)) {
+        mysqli_stmt_bind_param($stmt, "iisi",  $wine_id, $rate, $comment, $user_id);
+        mysqli_stmt_execute($stmt);
+
+    }
+    $sql2 = "SELECT r.rate, r.wine_id, r.comment, w.wine_name, u.username,
+    ROUND((SELECT AVG(rate) FROM review WHERE wine_id = r.wine_id), 1) AS average_rate
+    FROM review r
+    JOIN wine w ON r.wine_id = w.wine_id
+    JOIN user u ON r.user_id = u.user_id
+    WHERE r.wine_id = $wine_id";
+
+    $result = $conn->query($sql2);
+    $data = array();
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+    }
+    $send = array(
+        "type" => "AfterAdding",
+        "data" => $data
+    );
+
+   
+    header('Content-Type: application/json');
+    echo json_encode( $send);
+
+}
+else if (isset($json['view']))
+{
+    $sql2 = "SELECT r.review_id, r.wine_id, r.comment, w.wine_name, ROUND(AVG(r.rate), 1) AS average_rate
+    FROM review r
+    JOIN wine w ON w.wine_id = r.wine_id
+   
+    GROUP BY r.review_id, r.wine_id, r.comment, w.wine_name";
+
+    $result = $conn->query($sql2);
+    $ReviewItems = array();
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $ReviewItems[] = $row;
+        }
+    }
+    $send = array(
+        "type" => "Viewing",
+        "data" => $ReviewItems
+    );
+
+   
+    header('Content-Type: application/json');
+    echo json_encode( $send);
+
+
+        
+}
+if(isset($json['BasedOnwineADD'])) {
+///////////////////////////////////////////////////////////////////
+//checking if the user is allowed to make a review
+    $wine_id = $json['wine_id'];
+    $user_id = $json['user_id'];
+    //check if user is a customer
+    $sqlCheck="SELECT customer_id FROM customer Where user_id=$user_id LIMIT 1";
+    $result = $conn->query($sqlCheck);
+   
+        $rowCount = mysqli_num_rows($result);
+        if ($rowCount=== 0) {
+            $send = array(
+                "type" => "Notcustomer",
+                "message"=>"User has never purchased before therefore user cannot write a review."
+                
+            );
+          
+            header('Content-Type: application/json');
+            echo json_encode( $send);
+        }
+        else{
+            //extracting customer id
+            //add users review
+            $row = mysqli_fetch_assoc($result);
+            $customer_id = $row['customer_id']; 
+         
+            if(isset($json['comment']))
+            {
+                $comment = $json['comment'];
+            }
+            else
+            {
+                $comment ="no comment";
+            }
+            if(isset($json['rate']) && $json['rate']!=null )
+            {
+                $rate = $json['rate'];
+            }
+            else
+            {
+                $rate=0;
+            }
+          
+            
+               
+            
+    
+
+            $sql1 = 'INSERT INTO review ( wine_id, rate, comment, customer_id)
+            VALUES (?, ?, ?, ?)';
+
+    $stmt = mysqli_stmt_init($conn);
+
+    if (mysqli_stmt_prepare($stmt, $sql1)) {
+        mysqli_stmt_bind_param($stmt, "iisi",  $wine_id, $rate, $comment, $customer_id);
+        mysqli_stmt_execute($stmt);
+    }
+    ////////////////////////////////////////////////////
+    //showing updated review based on wine id
+    $sql2 = "SELECT rate, wine_id, comment,
+    (SELECT wine_name FROM wine WHERE wine_id = $wine_id) AS wine_name,
+    ROUND((SELECT AVG(rate) FROM review WHERE wine_id =$wine_id), 1) AS average_rate
+FROM review
+WHERE wine_id = $wine_id";
+
+    $result = $conn->query($sql2);
+    $rowCount = mysqli_num_rows($result);
+    ////////no results
+    if($rowCount===0)
+    {
+        $send = array(
+            "type" => "NoReviews",
+            "message"=>"No reviews on this wine has been made."
+            
+        );
+      
+        header('Content-Type: application/json');
+        echo json_encode( $send); 
+    }
+    else
+    { ///if there is results
+        $ReviewItems = array();
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $ReviewItems[] = $row;
+            }
+        }
+    
+        $send = array(
+            "type" => "BasedonWineAdded",
+            "data" => $ReviewItems
+        );
+  
+    
+    header('Content-Type: application/json');
+    echo json_encode( $send);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+   
+
+}
 
 
 
